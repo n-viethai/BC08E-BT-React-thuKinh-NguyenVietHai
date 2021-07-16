3c9dcbe1a58e7a7b6bb583530b294683e271c54f
HaiNguyen <viethai1309@gmail.com>
2021-07-16 19:13:23 +0700

    create project

---
+++ src/components/BaiTapThuKinh/BTThuKinh.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BTThuKinh extends Component {
+    render() {
+        return (
+            <div>
+                hahahah
+            </div>
+        )
+    }
+}
